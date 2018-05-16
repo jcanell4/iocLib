3dd2f352f858fdf641ae5ea94cce35c77927a928
rclaver <rclaver@xtec.cat>
2018-05-16 12:58:09 +0200

    centralizar las clases de excepciones

---
+++ wikiiocmodel/DefaultProjectModelExceptions.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * WikiIocModelException: Excepciones del proyecto 'defaultProject'
+ *
+ * @author Josep Cañellas <jcanell4@ioc.cat>
+ */
+if (!defined('DOKU_INC')) die();
+require_once(DOKU_INC . 'inc/common.php');
+
+class PageNotFoundException extends WikiIocModelException {
+    public function __construct($page, $codeMessage = "pageNotFound", $code=7101, $previous=NULL) {
+        parent::__construct($codeMessage, $code, $previous, $page);
+    }
+}
+
+class PageAlreadyExistsException extends WikiIocModelException {
+    public function __construct($page, $message='pageExist', $code=7102, $previous=NULL) {
+        parent::__construct($message, $code, $previous, $page);
+    }
+}
+
+class DateConflictSavingException extends WikiIocModelException {
+    public function __construct($page, $codeMessage = "conflictsSaving", $code=7103, $previous=NULL) {
+        parent::__construct($codeMessage, $code, $previous, $page);
+    }
+}
+
+class WordBlockedException extends WikiIocModelException {
+    public function __construct($page, $codeMessage = "wordblock", $code=7104, $previous=NULL) {
+        parent::__construct($codeMessage, $code, $previous, $page);
+    }
+}
+
+class CommandAuthorizationNotFound extends WikiIocModelException {
+    public function __construct($param='', $message='commandAuthorizationNotFound', $code=7105, $previous=NULL) {
+        parent::__construct($message, $code, $previous, $param);
+    }
+}
+
+class InsufficientPermissionToUploadMediaException extends WikiIocModelException {
+    public function __construct($param='', $codeMessage = 'auth_UploadMedia', $code=7106, $previous=NULL) {
+        parent::__construct($codeMessage, $code, $previous, $param);
+    }
+}
+
+class CantCreatePageInProjectException extends WikiIocModelException {
+    public function __construct($param='', $message='cantCreatePageInProject', $code=7107, $previous=NULL) {
+        parent::__construct($message, $code, $previous, $param);
+    }
+}
+
+class FailToUploadMediaException extends WikiIocModelException {
+    public function __construct($errorCode, $codeMessage = 'uploadfail', $code=7108, $previous=NULL) {
+        parent::__construct($codeMessage, $code, $previous, $errorCode);
+    }
+}
+
+class MaxSizeExcededToUploadMediaException extends WikiIocModelException {
+    public function __construct($param='', $codeMessage = 'auth_UploadMedia', $code=7109, $previous=NULL) {
+        if(!$codeMessage){
+            $codeMessage = sprintf(WikiIocLangManager::getLang('uploadsize'),
+                    filesize_h(php_to_byte(ini_get('upload_max_filesize'))));
+        }
+        parent::__construct($codeMessage, $code, $previous, $param);
+    }
+}
+
+class InvalidUserException extends WikiIocModelException {
+    public function __construct($user, $message='Aquest usuari no és vàlid.', $code=7110, $previous=NULL) {
+        parent::__construct($message, $code, $previous, $user);
+    }
+}
+
+class IncorrectParamsException extends WikiIocModelException {
+    public function __construct($message='Paràmetres incorrectes.', $code=7110, $previous=NULL) {
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ wikiiocmodel/ProjectModelExceptions.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * ProjectModelException: Establece las clases de excepciones generales para proyectos
+ * @culpable Rafael Claver
+ */
+if (!defined('DOKU_INC')) die();
+
+class ProjectExistException extends WikiIocProjectException {
+    public function __construct($page, $message='projectExist', $code=7201) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class ProjectNotExistException extends WikiIocProjectException {
+    public function __construct($page, $message='projectNotExist', $code=7202) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class UnknownProjectException extends WikiIocProjectException {
+    public function __construct($page, $message='unknownProject', $code=7203) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class UserNotAuthorizedException extends WikiIocProjectException {
+    public function __construct($page, $message='userNotAuthorized', $code=7204) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class AuthorNotVerifiedException extends WikiIocProjectException {
+    public function __construct($page, $message='authorNotVerified', $code=7205) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class ResponsableNotVerifiedException extends WikiIocProjectException {
+    public function __construct($page, $message='responsableNotVerified', $code=7206) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class InsufficientPermissionToEditProjectException extends WikiIocProjectException {
+    public function __construct($page, $message='insufficientPermissionToEditProject', $code=7207) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class InsufficientPermissionToCreateProjectException extends WikiIocProjectException {
+    public function __construct($page, $message='insufficientPermissionToCreateProject', $code=7208) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class InsufficientPermissionToDeleteProjectException extends WikiIocProjectException {
+    public function __construct($page, $message='insufficientPermissionToDeleteProject', $code=7209) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class InsufficientPermissionToGenerateProjectException extends WikiIocProjectException {
+    public function __construct($page, $message='insufficientPermissionToGenerateProject', $code=7210) {
+        parent::__construct("$message$page", $code, $page, NULL, 'projectException');
+    }
+}
+
+class MissingGroupFormBuilderException extends WikiIocProjectException {
+    public function __construct($page='', $message='MissingGroupFormBuilder', $code=7301) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class MissingValueFormBuilderException extends WikiIocProjectException {
+    public function __construct($page='', $message='MissingValueFormBuilder', $code=7302) {
+        parent::__construct($message, $code, $page, NULL, 'projectException');
+    }
+}
+
+class WrongNumberOfColumnsFormBuilderException extends WikiIocProjectException {
+    public function __construct($page='', $message='nombre incorrecte de', $code=7303) {
+        parent::__construct("Has indicat $message columnes i el nombre de columnes admés està entre 1 i 12", $code, $page);
+    }
+}
