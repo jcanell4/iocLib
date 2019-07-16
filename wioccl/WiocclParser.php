73f2064f604ad27e06e99a9912132784ae599278
Xavier García <xaviergaro.dev@gmail.com>
2019-07-16 12:46:44 +0200

    Fixed upgrader to use readonly blocks and removed protected tags before the parsing

--- wioccl/WiocclParser.php
+++ wioccl/WiocclParser.php
@@ -2,9 +2,11 @@
 
 class WiocclParser
 {
-//    protected $instruction;
-    
-    /* TODO: els noms dels WIOOCCL s'extrauran automàticament */
+
+    protected static $removeTokenPatterns = [
+        '/:###/', '/###:/'
+    ];
+
     protected static $tokenPatterns = [
         '{@@' => [
             'state' => 'open_extra',
@@ -110,30 +112,14 @@ class WiocclParser
         '</WIOCCL:DEFAULTASE>' => ['state' => 'close_set', 'type' => 'case', 'action' => 'close'],
     ];
 
-
-//    // TODO: Afegir dataSource al constructor, deixem els arrays separats perque el seu us es intern, al datasource es ficaran com a JSON
-//    public function __construct($value = null, $arrays = [], $dataSource = []){
-////        $this->rawValue = $value;
-////        $this->arrays += $arrays;
-////        $this->dataSource = $dataSource; // TODO: Reactivar quan es comprovi que funciona
-//        $this->instruction = new WiocclInstruction($value, $arrays, $dataSource);
-//    }
-
-
     public static function getValue($text = null, $arrays = [], $dataSource = [])
     {
+        $replacements = array_fill(0, count(self::$removeTokenPatterns), '');
+
+        $text = preg_replace(self::$removeTokenPatterns, $replacements, $text);
+
         return self::parse($text, $arrays, $dataSource);
     }
-
-//    public function getRender()
-//    {
-//        return '<mark>' . $this->parse($this->rawValue) . '</mark>';
-//    }
-
-//    public function getTokensValue($tokens, &$tokenIndex)
-//    {
-//        return $this->parseTokens($tokens, $tokenIndex);
-//    }
 
     public static function parse($text = null, $arrays = [], $dataSource = [])
     {
@@ -142,10 +128,6 @@ class WiocclParser
         return $instruction->parseTokens($tokens); // això retorna un únic valor amb els valor dels tokens concatenats
     }
 
-//    protected function getContent($token)
-//    {
-//        return $token['value'];
-//    }
 
     protected static function tokenize($rawText)
     {
@@ -209,131 +191,4 @@ class WiocclParser
 
         return $token;
     }
-
-//    protected function parseTokens($tokens, &$tokenIndex = 0)
-//    {
-//
-//        $result = '';
-//
-//        while ($tokenIndex < count($tokens)) {
-//            $newChunk = $this->parseToken($tokens, $tokenIndex);
-//            if ($newChunk === null) { // tancament del wiocclXXX
-//                break;
-//            }else{
-//                $result .= $newChunk ;
-//            }
-//            ++$tokenIndex;
-//        }
-//
-//        return $result;
-//    }
-//
-//    // l'index del token analitzat s'actualitza globalment per referència
-//    protected function parseToken($tokens, &$tokenIndex)
-//    {
-//
-//        $currentToken = $tokens[$tokenIndex];
-//        $result = '';
-//
-//        if ($currentToken['state'] == 'content') {
-//            $action = 'content';
-//        } else {
-//            $action = $currentToken['action'];
-//        }
-//
-//
-//        switch ($action) {
-//            case 'content':
-//                $result .= $this->getContent($currentToken);
-//                break;
-//
-//            case 'open':
-//                $mark = self::$instancesCounter==0;
-//                self::$instancesCounter++;
-//                $item = $this->getClassForToken($currentToken);
-//                if($mark){
-//                    $result .= $item->getTokensValue($tokens, ++$tokenIndex);
-////                    $result .= "<mark title='${$this->fullInstruction}'>".$item->getTokensValue($tokens, ++$tokenIndex)."</mark>";
-//                }else{
-//                    $result .= $item->getTokensValue($tokens, ++$tokenIndex);
-//                }
-//                self::$instancesCounter--;
-//                break;
-//
-//            case 'close':
-//                return null;
-//                break;
-//        }
-//
-//        return $result;
-//    }
-//
-//    protected function getClassForToken($token)
-//    {
-//        // TODO: pasar el datasource i els arrays al constructor
-//        return new $token['class']($token['value'], $this->getArrays(), $this->getDataSource());
-//    }
-//
-//    protected function normalizeArg($arg)
-//    {
-//        if (strtolower($arg) == 'true') {
-//            return true;
-//        } else if (strtolower($arg) == 'false') {
-//            return false;
-//        } else if (is_int($arg)) {
-//            return intval($arg);
-//        } else if (is_numeric($arg)) {
-//            return floatval($arg);
-//        } else if (preg_match("/^''(.*?)''$/", $arg, $matches) === 1) {
-//            return $this->normalizeArg($matches[1]);
-//        } else {
-//            return $arg;
-//        }
-//
-//    }
-//
-//    protected function extractNumber($value, $attr, $mandatory=true) {
-//        $ret = 0;
-//        if (preg_match('/'.$attr.'="(.*?)"/', $value, $matches)) {
-//            $ret = (new WiocclParser($matches[1], $this->getArrays(), $this->getDataSource()))->getValue();
-//        } else if($mandatory){
-//            throw new Exception("$attr is missing");
-//        }
-//        if (is_numeric($ret)) {
-//            $ret = intval($ret);
-//        }
-//        return $ret;
-//    }
-//    
-//    protected function extractVarName($value, $attr="var", $mandatory=true) {
-//        if (preg_match('/'.$attr.'="(.*?)"/', $value, $matches)) {
-//            return $matches[1];
-//        } else if($mandatory){
-//            throw new Exception("$attr name is missing");
-//        }
-//        return "";
-//    }
-//
-//    protected function extractArray($value, $attr="array", $mandatory=true) {
-//        $jsonString = '[]';
-//        // ALERTA: El $value pot ser un json directament o una variable, s'ha de fer un parse del $value
-//        if (preg_match('/'.$attr.'="(.*?)"/', $value, $matches)) {
-//            $jsonString = (new WiocclParser($matches[1], $this->getArrays(), $this->getDataSource()))->getValue();
-//        } else if($mandatory){
-//            throw new Exception("Array is missing");
-//        }
-//        return json_decode($jsonString, true);
-//    }
-//
-//    public function getDataSource(){
-//        return $this->dataSource;
-//    }
-//
-//    public function setArrayValue($key, $value){
-//        $this->arrays[$key] = $value;
-//    }
-//    
-//    public function getArrays(){
-//        return $this->arrays;
-//    }
 }
