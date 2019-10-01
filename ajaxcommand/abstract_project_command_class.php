0187733a80db825453f8ddaafc7dd2e5ccb561fa
rclaver <rclaver@xtec.cat>
2019-10-01 10:31:05 +0200

    borrar comentarios

--- ajaxcommand/abstract_project_command_class.php
+++ ajaxcommand/abstract_project_command_class.php
@@ -13,16 +13,15 @@ abstract class abstract_project_command_class extends abstract_command_class {
         $this->types[ProjectKeys::KEY_DO] = self::T_STRING;
         //Set default values to $params
         $this->setParameters([ProjectKeys::KEY_DO => ProjectKeys::KEY_VIEW,
-                              ProjectKeys::KEY_METADATA_SUBSET => ProjectKeys::VAL_DEFAULTSUBSET]);
-//        $this->setParameters([ProjectKeys::KEY_DO => ProjectKeys::KEY_PARTIAL,
-//            ProjectKeys::KEY_METADATA_SUBSET => ProjectKeys::VAL_DEFAULTSUBSET]);
+                              ProjectKeys::KEY_METADATA_SUBSET => ProjectKeys::VAL_DEFAULTSUBSET
+                            ]);
     }
 
     public function init( $modelManager = NULL ) {
         parent::init($modelManager);
         $id = ($this->params[ProjectKeys::KEY_NS]) ? $this->params[ProjectKeys::KEY_NS] : $this->params[ProjectKeys::KEY_ID];
         $projectMetaDataQuery = $this->getPersistenceEngine()->createProjectMetaDataQuery(
-                                                                    $id, 
+                                                                    $id,
                                                                     $this->params[ProjectKeys::KEY_METADATA_SUBSET],
                                                                     $this->params[ProjectKeys::KEY_PROJECT_TYPE]);
         $this->dataProject = $projectMetaDataQuery->getDataProject();
