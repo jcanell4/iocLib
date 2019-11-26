d3e5436d589167b2c65ea356eeb3034a8838184f
rclaver <rclaver@xtec.cat>
2019-11-26 14:40:20 +0100

    $requestError

--- webservice/WsMoodleClient.php
+++ webservice/WsMoodleClient.php
@@ -12,71 +12,74 @@
  * @author josep
  */
 class WsMoodleClient {
-    protected $token;
+    protected $token = NULL;
     protected $wsFunction;
     protected $moodlewsrestformat='json';
     protected $urlBase = "https://ioc.xtec.cat/campus";
     protected $furlToken = "/login/token.php";
     protected $furl ="/webservice/rest/server.php";
     protected $urlParams=array();
-    
+    protected $requestError = NULL;
+
     public function init($urlBase, $furlToken, $furl, $urlParams=false){
         $this->url = $url;
         if(is_array($urlParams)){
             $this->urlParams = array_merge([], $urlParams);
         }
     }
-    
+
     public function setToken($token){
         $this->token=$token;
     }
-            
+
     public function setWsFunction($wsFunction){
         $this->wsFunction = $wsFunction;
     }
-            
+
     public function setUrlParams(array $urlParams){
         if(is_array($urlParams)){
             $this->urlParams = array_merge([], $urlParams);
         }
     }
-    
+
     public function sendRequest(array $wsParams, $wsFunction=FALSE, $moodlewsrestformat='json'){
         $url = $this->urlBase.$this->furl;
         if(!$wsFunction){
             $wsFunction = $this->wsFunction;
         }
         $query = http_build_query(["wstoken" => $this->token, "wsfunction" => $wsFunction, "moodlewsrestformat" => $moodlewsrestformat], "", "&");
-        
+
         $postData = $this->getStrData($wsParams);
-        
+
         return $this->_sendRequest($url, "", $query."&".$postData);
-                     
+
     }
-    
+
     public function updateToken($user, $pass){
         $url = $this->urlBase. $this->furlToken;
         $query = http_build_query(["username" => $user, "password" => $pass, "service" => "moodle_mobile_app"], "", "&");
         $result = json_decode($this->_sendRequest($url, "", $query));
+        $this->requestError = ($result->error) ? $result : NULL;
         $this->setToken($result->token);
     }
-    
+
     public function getToken(){
         return $this->token;
     }
-    
+
     protected function _sendRequest($url, $query="", $postData=FALSE){
-        if($query){
+        $this->token = NULL;
+        if ($query){
             $url = $url."?".$query;
         }
-        if($postData){
+        if ($postData){
             $context = $this->getContext("POST", $postData);
         }else{
             $context = $this->getContext("GET");
         }
         return file_get_contents($url, false, $context);
     }
-    
+
     protected function getStrData($data){
         if(is_array($data)){
             $query  = http_build_query($data, "","&");
@@ -86,7 +89,7 @@ class WsMoodleClient {
         }
         return $query;
     }
-    
+
     protected function getContext($method="GET", $data=FALSE){
         $context = [
             'http' => [
@@ -102,7 +105,7 @@ class WsMoodleClient {
             $context["http"]["header"] .= "Content-Length: ".strlen($data)."\r\n";
             $context["http"]["content"] = $data;
         }
-        
+
         return stream_context_create($context);
     }
 }
