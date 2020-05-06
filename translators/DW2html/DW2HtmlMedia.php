87abb68519346e14768dd6a79e940b325359075d
Xavier García <xaviergaro.dev@gmail.com>
2020-05-06 18:36:51 +0200

    Partial push dojo video plugin

--- translators/DW2html/DW2HtmlMedia.php
+++ translators/DW2html/DW2HtmlMedia.php
@@ -3,6 +3,7 @@ require_once "DW2HtmlParser.php";
 
 class DW2HtmlMedia extends DW2HtmlImage {
 
+    private static $counter = 0;
 
     protected $urlPattern = "/{{(vimeo|youtube|dailymotion|altamarVideos)>(.*?)\|.*}}/";
 
@@ -32,18 +33,24 @@ class DW2HtmlMedia extends DW2HtmlImage {
         // what size?
         list($id, $param) = explode('?',$id,2);
         if(preg_match('/(\d+)x(\d+)/i',$param,$m)){
+            // TODO: No implementat al client
             // custom
-            $width  = $m[1];
-            $height = $m[2];
+//            $width  = $m[1];
+//            $height = $m[2];
+//            $size = "custom";
+
         }elseif(strpos($param,'small') !== false){
             // small
+            $size = 'small';
             $width  = 255;
             $height = 210;
         }elseif(strpos($param,'large') !== false){
             // large
+            $size = 'large';
             $width  = 520;
             $height = 406;
         }else{
+            $size = 'medium';
             // medium
             $width  = 425;
             $height = 350;
@@ -52,10 +59,10 @@ class DW2HtmlMedia extends DW2HtmlImage {
 //        return array($site, $url, $title, $width, $height);
 
 
-        if (preg_match($token['pattern'], $token['raw'], $match)) {
-//            $type = $match[1];
-            $id = $match[1];
-        }
+//        if (preg_match($token['pattern'], $token['raw'], $match)) {
+////            $type = $match[1];
+//            $id = $match[1];
+//        }
 
 
         // TODO[Xavi] Cal incloure les etiquetes de altamarVideos
@@ -81,8 +88,18 @@ class DW2HtmlMedia extends DW2HtmlImage {
 
 
 
-        $html = '<div data-dw-block="video" data-video-type="' . $type . '" data-video-id="' . $id . '" data-ioc-id="ioc_video_' . $id . '" contenteditable="false">' .
-            '<iframe src="' . $url . '" width="' . $width .'" height="' . $height .'"></iframe>' .
+        // afegim un nombre aleatori al data-ioc-id per assegurar que no hi ha conflictes encara que es trobin 2 vídeos amb el mateix id real (el que s'envia al iframe)
+
+        try {
+            $random = rand(0, PHP_INT_MAX);
+        } catch (Exception $e) {
+            $random = (new DateTime())->getTimestamp() + self::$counter;
+            self::$counter++; // Cal assegurar-nos que aquest nombre será diferent encara que es cridi múltiples vegades
+        }
+
+
+        $html = '<div data-dw-block="video" data-video-type="' . $type . '" data-video-id="' . $id . '" data-ioc-id="ioc_video_' . $id . $random . '" contenteditable="false" data-video-title="'. $title .'" data-video-size="'. $size .'">' .
+            '<iframe src="' . $url . '" width="' . $width .'" height="' . $height .' title="' . $title . '"></iframe>' .
             '</div>';
 
         return $html;
