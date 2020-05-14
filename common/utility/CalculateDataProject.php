2e55dd2e6f86795efc853ce30deeb6bd03903b5e
rclaver <rclaver@xtec.cat>
2020-05-14 12:46:08 +0200

    processAutoFieldsOnRead i CalculateDataProject

--- common/utility/CalculateDataProject.php
+++ common/utility/CalculateDataProject.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Description of CalculateDataProject
  *
@@ -8,7 +7,9 @@
 class CalculateDataProject extends CalculateWithPersistence{
 
     public function calculate($data) {
-        $values = $this->getPersistence()->createProjectMetaDataQuery($data[ProjectKeys::KEY_PROJECT_ID], $data["subSet"])->getDataProject();
+        $values = $this->getPersistence()
+                        ->createProjectMetaDataQuery($data[ProjectKeys::KEY_PROJECT_ID], $data[ProjectKeys::KEY_METADATA_SUBSET])
+                        ->getDataProject();
         $ret = $values[$data["field"]];
         return $ret;
     }
