5bfd5cf44488fd5751cf7e143e21d8e70286bc85
rclaver <rclaver@xtec.cat>
2020-05-15 13:00:49 +0200

    CalculateDataProject nova versió: accepta camps amb estructura

--- common/utility/CalculateDataProject.php
+++ common/utility/CalculateDataProject.php
@@ -10,7 +10,23 @@ class CalculateDataProject extends CalculateWithPersistence{
         $values = $this->getPersistence()
                         ->createProjectMetaDataQuery($data[ProjectKeys::KEY_PROJECT_ID], $data[ProjectKeys::KEY_METADATA_SUBSET])
                         ->getDataProject();
+
         $ret = $values[$data["field"]];
+
+        if (!$ret) {
+            $components = explode("#", $data["field"]);
+            $field = array_shift($components);
+            $ret = $values[$field];
+            if ($ret) {
+                foreach ($components as $field) {
+                    $ret = $ret[$field];
+                }
+            }
+        }
+
+        if (!$ret)
+            throw new Exception("Error: No s'ha trobat {$data["field"]} definit a configMain");
+
         return $ret;
     }
 }
