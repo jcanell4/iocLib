ced124dd8a088af82a3844e559fd74d8fe9f6094
Xavier García <xaviergaro.dev@gmail.com>
2020-05-26 15:44:04 +0200

    Fixed floating comment

--- translators/DW2html/DW2HtmlImageGIF.php
+++ translators/DW2html/DW2HtmlImageGIF.php
@@ -4,7 +4,7 @@ require_once "DW2HtmlParser.php";
 class DW2HtmlImageGIF extends DW2HtmlImage {
 
 
-    protected $urlPattern = "/{{iocgif>(.*?)(?:\|.*?)?}}/";
+//    protected $urlPattern = "/{{iocgif>(.*?)(?:\|.*?)?}}/";
 
     public function open() {
 
@@ -34,12 +34,12 @@ class DW2HtmlImageGIF extends DW2HtmlImage {
     protected function extractUrl($token, &$width = 0, &$height = 0, &$CSSclasses = '', &$ns, &$isInternal = false) {
         // A diferencia dels enllaços la URL si conté un punt, el que separa la extensió.
         // un enllaç extern només pot contenir 2 punts per separar el protocol, eliminem aquesta posibilitiat
-        $testUrl = str_replace('https:', '', $token['raw']);
-        $testUrl = str_replace('http:', '', $testUrl);
-        $testUrl = str_replace('ftp:', '', $testUrl);
+//        $testUrl = str_replace('https:', '', $token['raw']);
+//        $testUrl = str_replace('http:', '', $testUrl);
+//        $testUrl = str_replace('ftp:', '', $testUrl);
 
 
-        preg_match($this->urlPattern, $token['raw'], $matchUrl);
+        preg_match("/{{iocgif>(.*?)(?:\|.*?)?}}/", $token['raw'], $matchUrl);
         $candidateUrl = $matchUrl[1];
 
         if (strpos($candidateUrl, '?')) {
@@ -58,17 +58,17 @@ class DW2HtmlImageGIF extends DW2HtmlImage {
 
 
         // Si és un enllaç ha de contenir com a mínim una barra \
-        if (strpos($testUrl, '\|') !== false) {
-
-            $urlPattern = "/{{(.*?) ?\|?.*?}}/";
-            preg_match($urlPattern, $token['raw'], $matchUrl);
-            $url = trim($candidateUrl);
-
-
-        } else {
+//        if (strpos($testUrl, '\|') !== false) {
+//
+//            $urlPattern = "/{{(.*?) ?\|?.*?}}/";
+//            preg_match($urlPattern, $token['raw'], $matchUrl);
+//            $url = trim($candidateUrl);
+//
+//
+//        } else {
             $url = "lib/exe/fetch.php?media=" . trim($candidateUrl);
             $isInternal = true;
-        }
+//        }
 
         return $url;
 
