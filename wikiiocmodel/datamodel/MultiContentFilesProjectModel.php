49ceb56d647fda2a86c8ec480f44b51e1f87c863
rclaver <rclaver@xtec.cat>
2020-07-03 15:15:02 +0200

    la definición de createTemplateDocument($data=NULL) debe coincidir con la herencia

--- wikiiocmodel/datamodel/MultiContentFilesProjectModel.php
+++ wikiiocmodel/datamodel/MultiContentFilesProjectModel.php
@@ -1,22 +1,16 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of MultiContentFilesProjectModel
  *
  * @author josep
  */
 abstract class MultiContentFilesProjectModel extends AbstractProjectModel{
-    public function createTemplateDocument($data){
+
+    public function createTemplateDocument($data=NULL){
         $pdir = $this->getProjectMetaDataQuery()->getProjectTypeDir()."metadata/plantilles/";
         $scdir = scandir($pdir);
         foreach($scdir as $file){
-            if ($file !== '.' && $file !== '..' && substr($file, -4)===".txt") {
+            if ($file !== '.' && $file !== '..' && substr($file, -4) === ".txt") {
                 $plantilla = file_get_contents($pdir.$file);
                 $name = substr($file, 0, -4);
                 $this->dokuPageModel->setData([PageKeys::KEY_ID => $this->id.":".$name,
