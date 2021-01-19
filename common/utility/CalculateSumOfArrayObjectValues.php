ef0460b9c702f24a13d6f874a8c68875b0ba4d4e
rclaver <rclaver@xtec.cat>
2021-01-19 18:37:17 +0100

    coorección: castToArray($var, $ThrowException=TRUE) eliminar espacios en blanco al final de línea

--- common/utility/CalculateSumOfArrayObjectValues.php
+++ common/utility/CalculateSumOfArrayObjectValues.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Description of CalculateSumOfArrayObjectValues. Aquesta classe retorna la suma 
+ * Description of CalculateSumOfArrayObjectValues. Aquesta classe retorna la suma
  * d'un subcamp d'un camp de tipus ArrayObject del propi projecte (values)
  * EL paràmetre d'entada ha de tenir la següent estructura:
  *  - arrayObjectfield: és el camp de l'objecte que conté l'arrayObject on practicar la suma
@@ -9,22 +8,22 @@
  *
  * @author josep
  */
-class CalculateSumOfArrayObjectValues extends CalculateFromValues{
+class CalculateSumOfArrayObjectValues extends CalculateFromValues {
     const ARRAY_OBJECT_FIELD_PARAM = "arrayObjectfield";
-    const FIELD_TO_ADD_PARAM ="fieldToAdd";
-    //put your code here
-    public function calculate($data) {        
+    const FIELD_TO_ADD_PARAM = "fieldToAdd";
+
+    public function calculate($data) {
         $arrayObjectfield = $this->getParamValue($data[self::ARRAY_OBJECT_FIELD_PARAM]);
-        $values= $this->setVariable(self::ARRAY_OBJECT_VALUE_VAR, $this->castToArray($this->getValues()[$arrayObjectfield]));
-        $sum =0;
-        if(is_array($values)){
+        $values = $this->setVariable(self::ARRAY_OBJECT_VALUE_VAR, $this->castToArray($this->getValues()[$arrayObjectfield], FALSE));
+        $sum = 0;
+        if (is_array($values)) {
             foreach ($values as $item) {
                 $this->setVariable(self::ROW_VALUE_VAR, $item);
                 $fieldToAdd = $this->getParamValue($data[self::FIELD_TO_ADD_PARAM]);
                 $sum += $item[$fieldToAdd];
             }
         }
-        return $sum; 
+        return $sum;
     }
 
 }
