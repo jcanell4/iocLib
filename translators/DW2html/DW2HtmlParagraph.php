ddfa8e76ac67593ad1e5e18945771e7218d69f04
Xavier Garcia <xaviergaro.dev@gmail.com>
2021-02-16 08:24:45 +0100

    Fix new paragraph on single \n (one is consumed after </wioccl>)

--- translators/DW2html/DW2HtmlParagraph.php
+++ translators/DW2html/DW2HtmlParagraph.php
@@ -29,8 +29,9 @@ class DW2HtmlParagraph extends DW2HtmlInstruction {
     public function isClosing($token) {
         // un doble salt de línia sempre tanca un paràgraf
 
+        // Afegit el salt de línia simple, si no
         if ((isset($token['extra']) && $token['extra']['block'] === TRUE && $token['action'] == 'open')
-            || $token['raw'] === "\n\n") {
+            || $token['raw'] === "\n\n" || $token['raw'] === "\n") {
 
             return true;
         } else {
