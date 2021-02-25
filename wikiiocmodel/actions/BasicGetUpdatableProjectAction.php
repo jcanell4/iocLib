d0b1f9d1ceb627ac1537b160c9f69200ef281722
rclaver <rclaver@xtec.cat>
2021-02-25 14:36:17 +0100

    $response['info']

--- wikiiocmodel/actions/BasicGetUpdatableProjectAction.php
+++ wikiiocmodel/actions/BasicGetUpdatableProjectAction.php
@@ -4,11 +4,17 @@ if (!defined("DOKU_INC")) die();
 class BasicGetUpdatableProjectAction extends BasicGetProjectAction {
 
     protected function runAction() {
-        $response = parent::runAction();
         
         if (! $this->isUpdatedDate($this->params[ProjectKeys::KEY_METADATA_SUBSET])) {
             $this->getModel()->setViewConfigName("updateView");
+            $new_message = self::generateInfo("info", "El projecte no està actualitzat", $this->params[ProjectKeys::KEY_ID]);
         }
+
+        $response = parent::runAction();
+
+        if ($new_message)
+            $response['info'] = self::addInfoToInfo($response['info'], $new_message);
+        
         return $response;
     }
 
