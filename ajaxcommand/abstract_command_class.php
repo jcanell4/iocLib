7dda14c0fefe546f15cef41b23d598472c067f3f
rclaver <rclaver@xtec.cat>
2021-04-09 11:47:47 +0200

    control d'errors

--- ajaxcommand/abstract_command_class.php
+++ ajaxcommand/abstract_command_class.php
@@ -271,7 +271,7 @@ abstract class abstract_command_class extends DokuWiki_Plugin {
             $ret = $responseGenerator->getJsonResponse();
         }
         //for a dojo iframe the json response has to be inside a textarea
-        if (isset($this->params['iframe']) && empty($this->errorMessage)) {
+        if (isset($this->params['iframe']) && !$this->error) {
             $ret = "<html><body><textarea>" . $ret. "</textarea></body></html>";
         }
         $this->triggerEndEvents();
