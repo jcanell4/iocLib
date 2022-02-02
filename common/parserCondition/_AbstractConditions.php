2a761e63c3c9e42222937b1abc32fd977583cf44
Xavier Garcia <xaviergaro.dev@gmail.com>
2022-02-02 21:20:56 +0100

    Afegit parser per nodes agregats i condicionals

--- common/parserCondition/_AbstractConditions.php
+++ common/parserCondition/_AbstractConditions.php
@@ -1,56 +1,70 @@
 <?php
 
-class _BaseCondition {
+class _BaseCondition
+{
     protected $strCondition;
     public $logicOp;
 
     // Classe a partir de la qual s'invoquen els mètodes estàtics getValue() i parse()
     public $parser;
 
-    public function __construct($strCondition, $parser) {
+    public function __construct($strCondition, $parser)
+    {
         $this->strCondition = empty($strCondition) ? 'false' : $strCondition;
-        $this->logicOp = _LogicParser::getOperator($strCondition, $parser);
+        $this->logicOp = _ConditionLogicParser::getOperator($strCondition, $parser);
         $this->parser = $parser;
     }
 
 
-    public function parseData($arrays, $dataSource, &$resetables) {
+    // Canviar per;
+    // public function parseData($arrays, $dataSource = [], &$resetables = null) {
+    public function parseData($arrays, $dataSource, &$resetables)
+    {
         $this->logicOp->parseData($arrays, $dataSource, $resetables);
     }
 
-    public function validate() {
+    public function validate()
+    {
         return $this->logicOp->getValue();
     }
 
-    public function setValue1($value, $override = true) {
+    public function setValue1($value, $override = true)
+    {
         $this->logicOp->setValue1($value, $override);
     }
 
-    public function setValue2($value, $override = true) {
+    public function setValue2($value, $override = true)
+    {
         $this->logicOp->setValue2($value, $override);
     }
 
 
 }
 
-class _LogicParser {
+// Crear classe _LogicParser que faci de dispatcher
+
+
+// Factoria
+class _ConditionLogicParser
+{
 //    protected $text;
 //    function __construct($text) {
 //        $this->text = $text;
 //    }
 
-    public static function getOperator($text, $parser) {
+    public static function getOperator($text, $parser)
+    {
         $ret = NULL;
 //        if(preg_match('/\(.*\)/', $text, $matches) === 1){
 //
 //        }
         $aOrOp = explode("||", $text, 2);
         if (count($aOrOp) > 1) {//OR
-            $ret = new _OrOperation(_LogicParser::getOperator(trim($aOrOp[0]), $parser), _LogicParser::getOperator(trim($aOrOp[1]), $parser), $parser);
+            $ret = new _OrOperation(_ConditionLogicParser::getOperator(trim($aOrOp[0]), $parser), _ConditionLogicParser::getOperator(trim($aOrOp[1]), $parser), $parser);
         } else {//AND
             $aAndOp = explode("&&", $text, 2);
             if (count($aAndOp) > 1) {
-                $ret = new _AndOperation(_LogicParser::getOperator(trim($aAndOp[0]), $parser), _LogicParser::getOperator(trim($aAndOp[1]), $parser), $parser);
+                $ret = new _AndOperation(_ConditionLogicParser::getOperator(trim($aAndOp[0]), $parser), _ConditionLogicParser::getOperator(trim($aAndOp[1]), $parser), $parser);
             } else if (preg_match('/[=!]=/', $text) === 1) {//CONDITION == o !=
                 $ret = new _ConditionOperation($text, $parser);
             } else if (preg_match('/[><]=?/', $text) === 1) {//CONDITION <, >, <=, <=
@@ -67,7 +81,9 @@ class _LogicParser {
     }
 }
 
-abstract class _LogicOperation {
+
+abstract class _LogicOperation
+{
 
     protected $parser;
 
@@ -80,7 +96,8 @@ abstract class _LogicOperation {
 
     abstract function parseData($arrays, $datasource, &$resetables);
 
-    protected function normalizeArg($arg) {
+    protected function normalizeArg($arg)
+    {
         if (strtolower(trim($arg)) == 'true') {
             return true;
         } else if (strtolower(trim($arg)) == 'false') {
@@ -98,34 +115,41 @@ abstract class _LogicOperation {
     }
 }
 
-abstract class _BinaryOperation extends _LogicOperation {
+abstract class _BinaryOperation extends _LogicOperation
+{
     private $operator1;
     private $operator2;
 
-    function __construct($op1, $op2 = NULL, $parser) {
+    function __construct($op1, $op2 = NULL, $parser)
+    {
         $this->operator1 = $op1;
         $this->operator2 = $op2;
 
         parent::__construct($parser);
     }
 
-    public function getOperator1() {
+    public function getOperator1()
+    {
         return $this->operator1;
     }
 
-    public function getOperator2() {
+    public function getOperator2()
+    {
         return $this->operator2;
     }
 
-    public function setOperator1($operator1) {
+    public function setOperator1($operator1)
+    {
         $this->operator1 = $operator1;
     }
 
-    public function setOperator2($operator2) {
+    public function setOperator2($operator2)
+    {
         $this->operator2 = $operator2;
     }
 
-    public function parseData($arrays, $datasource, &$resetables) {
+    public function parseData($arrays, $datasource, &$resetables)
+    {
         $this->operator1->parseData($arrays, $datasource, $resetables);
         if ($this->operator2 !== NULL) {
             $this->operator2->parseData($arrays, $datasource, $resetables);
@@ -133,23 +157,26 @@ abstract class _BinaryOperation extends _LogicOperation {
     }
 }
 
-class _Literal extends _LogicOperation {
+class _Literal extends _LogicOperation
+{
     private $literal;
     private $value;
 
-    function __construct($text, $parser) {
+    function __construct($text, $parser)
+    {
         $this->literal = $this->normalizeArg($text);
 
         parent::__construct($parser);
     }
 
-    public function getValue() {
+    public function getValue()
+    {
 
         // Si el literal es true o false ho retornem, quan es fa el parser dels valors es converteixen en enters.
         if ($this->literal === TRUE || $this->literal === FALSE) {
             return $this->literal;
         } else {
-            return isset($this->value) ?  $this->normalizeArg($this->value) : TRUE;
+            return isset($this->value) ? $this->normalizeArg($this->value) : TRUE;
         }
 
 
@@ -157,7 +184,8 @@ class _Literal extends _LogicOperation {
         //return $this->value ? $this->normalizeArg($this->value) : true;
     }
 
-    public function parseData($arrays, $datasource, &$resetables) {
+    public function parseData($arrays, $datasource, &$resetables)
+    {
 //        $this->value = (new WiocclParser($this->literal, $arrays, $datasource))->getValue();
 
 //        $this->value = WiocclParser::getValue($this->literal, $arrays, $datasource, $resetables);
@@ -167,53 +195,66 @@ class _Literal extends _LogicOperation {
     }
 }
 
-class _NotOperation extends _BinaryOperation {
-    function __construct($operator1, $parser) {
+class _NotOperation extends _BinaryOperation
+{
+    function __construct($operator1, $parser)
+    {
         $no = substr($operator1, 1);
-        parent::__construct(_LogicParser::getOperator($no, $parser), NULL, $parser);
+        parent::__construct(_ConditionLogicParser::getOperator($no, $parser), NULL, $parser);
     }
 
-    public function getValue() {
+    public function getValue()
+    {
         return !$this->getOperator1()->getValue();
     }
+
 }
 
-class _AndOperation extends _BinaryOperation {
+class _AndOperation extends _BinaryOperation
+{
 
 //    function __construct($operator1, $operator2, $parser) {
 //        parent::__construct($operator1, $operator2, $parser);
 //    }
 
-    public function getValue() {
+    public function getValue()
+    {
         return $this->getOperator1()->getValue() && $this->getOperator2()->getValue();
     }
+
+    // TODO: Determinar si cal sobreescriure el parse data
 }
 
-class _OrOperation extends _BinaryOperation {
+class _OrOperation extends _BinaryOperation
+{
 
 //    function __construct($operator1, $operator2, $parser) {
 //        parent::__construct($operator1, $operator2, $parser);
 //    }
 
-    public function getValue() {
+    public function getValue()
+    {
         return $this->getOperator1()->getValue() || $this->getOperator2()->getValue();
     }
 }
 
-class _ConditionOperation extends _LogicOperation {
+class _ConditionOperation extends _LogicOperation
+{
     private $operation;
     private $arg1;
     private $arg2;
     private $value1;
     private $value2;
 
-    public function setValue1($value, $override = true) {
+    public function setValue1($value, $override = true)
+    {
         if ($this->value1 == NULL || $override) {
             $this->value1 = $value;
         }
     }
 
-    public function setValue2($value, $override = true) {
+    public function setValue2($value, $override = true)
+    {
 
         if ($this->value2 == NULL || $override) {
             $this->value2 = $value;
@@ -221,7 +262,8 @@ class _ConditionOperation extends _LogicOperation {
 
     }
 
-    function __construct($expression, $parser) {
+    function __construct($expression, $parser)
+    {
         $ac = $this->extractFilterArgs($expression);
         $this->arg1 = $ac[0];
         $this->arg2 = $ac[2];
@@ -230,7 +272,8 @@ class _ConditionOperation extends _LogicOperation {
         parent::__construct($parser);
     }
 
-    public function parseData($arrays, $datasource, &$resetables) {
+    public function parseData($arrays, $datasource, &$resetables)
+    {
 //        $this->value1 = $this->normalizeArg(WiocclParser::parse($this->arg1, $arrays, $datasource, $resetables));
 //        $this->value2 = $this->normalizeArg(WiocclParser::parse($this->arg2, $arrays, $datasource, $resetables));
 //
@@ -238,11 +281,13 @@ class _ConditionOperation extends _LogicOperation {
         $this->value2 = $this->normalizeArg($this->parser->parse($this->arg2, $arrays, $datasource, $resetables));
     }
 
-    public function getValue() {
+    public function getValue()
+    {
         return $this->resolveCondition($this->value1, $this->value2, $this->operation);
     }
 
-    protected function extractFilterArgs($value) {
+    protected function extractFilterArgs($value)
+    {
         if (preg_match('/(.*?)([><=!]={0,2}| in )(.*)/', $value, $matches) === 1) {
             // ALERTA: Actualment el token amb > arriba tallat perquè l'identifica com a tancament del token d'apertura
 
@@ -257,18 +302,18 @@ class _ConditionOperation extends _LogicOperation {
         return null;
     }
 
-    protected function resolveCondition($arg1, $arg2, $operator) {
-
+    protected function resolveCondition($arg1, $arg2, $operator)
+    {
         switch ($operator) {
 
             case '===':
                 return $arg1 === $arg2;
             case '==':
-                if(is_bool($arg1)&&$arg2==="null"){
-                    $arg2=false;
+                if (is_bool($arg1) && $arg2 === "null") {
+                    $arg2 = false;
                 }
-                if(is_bool($arg2)&&$arg1==="null"){
-                    $arg1=false;
+                if (is_bool($arg2) && $arg1 === "null") {
+                    $arg1 = false;
                 }
                 return $arg1 == $arg2;
             case '<=':
@@ -294,4 +339,89 @@ class _ConditionOperation extends _LogicOperation {
 
     }
 
+}
+
+// Gestió d'arbres
+
+// Representa un node fulla, el getValue sempre resoldrà una consulta lògia entre una o més condicions
+class Node {
+
+    public function getValue() {
+
+    }
+}
+
+
+
+abstract class _AggregationAbstractOperation extends _BinaryOperation
+{
+    function __construct($tree, $grups, $parser)
+    {
+
+        $operator1 = $this->getNode($tree, array_shift($grups));
+
+
+        if (count($grups)===1) {
+            $operator2 = $grups[0];
+        } else if (count($grups)>1) {
+            $operator2 = new static($tree, $grups, $parser);
+        } else {
+            $operator2 = NULL;
+        }
+
+        parent::__construct($operator1, $operator2, $parser);
+    }
+
+    private function getNode($tree, $key) {
+        return new Node($tree['grups'][$key]);
+    }
+}
+
+class _AggregationAndOperation extends _AggregationAbstractOperation
+{
+    public function getValue()
+    {
+        return $this->getOperator1()->getValue() && $this->getOperator2()->getValue();
+    }
+}
+
+class _AggregationOrOperation extends _AggregationAbstractOperation
+{
+    public function getValue()
+    {
+        return $this->getOperator1()->getValue() || $this->getOperator2()->getValue();
+    }
+}
+
+class _AggregationNullOperation extends _AggregationAbstractOperation
+{
+    public function getValue()
+    {
+        return $this->getOperator1()->getValue();
+    }
+}
+
+
+class _GroupLogicParser
+{
+    public static function getOperator($operator, $grups, $parser)
+    {
+        $ret = NULL;
+
+        switch ($operator) {
+            case 'or':
+                $ret = new _AggregationOrOperation($grups, $parser);
+                break;
+
+            case 'and':
+                $ret = new _AggregationAndOperation($grups, $parser);
+                break;
+
+            default:
+                // Només es fa servir el primer valor
+                $ret = new _AggregationNullOperation($grups[0], $parser);
+        }
+
+        return $ret;
+    }
 }
