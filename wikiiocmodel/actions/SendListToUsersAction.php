0be868c61af7247f82528576fda0e3687f4c88bd
rclaver <rclaver@xtec.cat>
2022-02-22 08:50:52 +0100

    tunning

--- wikiiocmodel/actions/SendListToUsersAction.php
+++ wikiiocmodel/actions/SendListToUsersAction.php
@@ -8,7 +8,6 @@ if (!defined("DOKU_INC")) die();
 class SendListToUsersAction extends NotifyAction {
 
     protected function responseProcess() {
-        $this->params['to'] = trim($this->params['users'], ",");
         $this->params['message'] .= ". Enllaç a la pàgina amb la llista de projectes filtrats.";
         $this->params["data-call"] = "selected_projects&grups=".str_replace('"', "'", $this->params['grups']);
 
