5b0fb4bb892529e4e35d4a9eb119264fd0bc20a4
rclaver <rclaver@xtec.cat>
2022-02-23 14:19:33 +0100

    temporal: Filtrar projectes per checkbox i afegir link als projectes seleccionats

--- wikiiocmodel/actions/SendListToUsersAction.php
+++ wikiiocmodel/actions/SendListToUsersAction.php
@@ -8,7 +8,11 @@ if (!defined("DOKU_INC")) die();
 class SendListToUsersAction extends NotifyAction {
 
     protected function responseProcess() {
-        $this->params['message'] .= ". Enllaç a la pàgina amb la llista de projectes filtrats.";
+        $this->params['message'] .= ".<br>Llista de projectes: ";
+        $checked_items = json_decode($this->params['checked_items'], true);
+        foreach ($checked_items as $ns) {
+            $this->params['message'] .= "<br>- <a href=".DOKU_URL."doku.php?id=$ns>$ns</a>";
+        }
         $this->params["data-call"] = "selected_projects&grups=".str_replace('"', "'", $this->params['grups']);
 
         $response['notifications'] = [];
