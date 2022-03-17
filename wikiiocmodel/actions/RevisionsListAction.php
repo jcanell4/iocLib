b5ca9185eab08b8eda8ce445341b7b3dc4df7034
rclaver <rclaver@xtec.cat>
2022-03-17 10:14:45 +0100

    primera correcció error en la selecció de "següents 50 revisions"

--- wikiiocmodel/actions/RevisionsListAction.php
+++ wikiiocmodel/actions/RevisionsListAction.php
@@ -11,6 +11,16 @@ class RevisionsListAction extends HtmlPageAction {
         parent::init($modelManager);
     }
 
+    protected function runProcess(){
+        $actual = WikiGlobalConfig::getConf('datadir')."/".str_replace(":","/",$this->params[PageKeys::KEY_ID])."/continguts.txt";
+        if (!WikiIocInfoManager::getInfo(WikiIocInfoManager::KEY_EXISTS) && !file_exists($actual)) {
+            throw new PageNotFoundException($this->params[PageKeys::KEY_ID]);
+        }
+        if (!WikiIocInfoManager::getInfo("perm")) {
+            throw new InsufficientPermissionToViewPageException($this->params[PageKeys::KEY_ID]);
+        }
+    }
+
     protected function responseProcess(){
         return $this->getRevisionList($this->params[PageKeys::KEY_OFFSET]);;
     }
