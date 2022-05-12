52a77c092b6cde188d57fc141c28ccca09406884
jcanell4 <jcanell4@xtec.cat>
2022-05-12 15:16:02 +0200

    Implementació de la crida al WsMix i el test de prova

--- webservice/WsMoodleClientTest.php
+++ webservice/WsMoodleClientTest.php
@@ -22,86 +22,181 @@ $ws = new WsMoodleClient();
 //
 //print(json_encode($token));
 
+//$token = "0c31efd2a78c176c92d929a044c376f2";
 $token = "0c31efd2a78c176c92d929a044c376f2";
-$ws->setToken($token);
-
-$params = [
-    "events" =>[
-        "eventids" =>[
-            0
-        ],
-        "groupids" =>[
-            0
-        ],
-        "courseids" =>[
-            2077
-        ]
-    ]
-];
-$res = $ws->sendRequest($params, "core_calendar_get_calendar_events");
-
-print(json_encode($res));
-$ids = array();
-
-$params = [
-     "events" =>array()
-];
-foreach ($res->events as $item){
-    if($item->eventtype=="course" && $item->courseid==2077){
-        $params["events"][] = ["eventid" => "{$item->id}", "repeat" => "1"];
-    }
-}
-
-if(count($params["events"])>0){
-    $res = $ws->sendRequest($params, "core_calendar_delete_calendar_events");
-    print(json_encode($res));
-}
-
-$date = new DateTime();
-$date->add(new DateInterval("P2D"));
-$d = date(DATE_RSS, $date->getTimestamp());
-$aparams = [
-    "events" =>[
-        [
-            "name" => "Esdeveniment de prova ($d)",
-            "timestart" => $date->getTimestamp(),
-            "courseid" => "2077",
-            "eventtype" => "course",
-            "description" => "Això és una prova per actualitzar el calendari des de la WIKI"
-        ]
-    ]
-];
-$res = $ws->sendRequest($aparams, "core_calendar_create_calendar_events");
-print(json_encode($res));
-
-$ws = new WsMoodleCalendar();
-
-$ws->setToken($token);
-//$res = $ws->getEventsForCourseId("2077");
+//$ws->setToken($token);
+//
+//$params = [
+//    "events" =>[
+//        "eventids" =>[
+//            0
+//        ],
+//        "groupids" =>[
+//            0
+//        ],
+//        "courseids" =>[
+//            2077
+//        ]
+//    ]
+//];
+//$res = $ws->sendRequest($params, "core_calendar_get_calendar_events");
+//
 //print(json_encode($res));
-
-$ws->deleteAllCourseEvents("2077");
-$events = [];
-$date = new DateTime();
-$date->add(new DateInterval("P2D"));
-$d = date(DATE_RSS, $date->getTimestamp());
-$events[] = EventMoodle::newInstanceFromAssociative(array(
-            "name" => "Esdeveniment de prova ($d)",
-            "timestart" => $date->getTimestamp(),
-            "eventtype" => "course",
-            "description" => "Això és una prova per actualitzar el calendari des de la WIKI"
-        ));
-$date->add(new DateInterval("P1D"));
-$d = date(DATE_RSS, $date->getTimestamp());
-$events[] = EventMoodle::newInstanceFromAssociative(array(
-            "name" => "Esdeveniment de prova ($d)",
-            "timestart" => $date->getTimestamp(),
-            "eventtype" => "course"
-        ));
-$ws->createEventsForCourseId("2077", $events);
+//$ids = array();
+//
+//$params = [
+//     "events" =>array()
+//];
+//foreach ($res->events as $item){
+//    if($item->eventtype=="course" && $item->courseid==2077){
+//        $params["events"][] = ["eventid" => "{$item->id}", "repeat" => "1"];
+//    }
+//}
+//
+//if(count($params["events"])>0){
+//    $res = $ws->sendRequest($params, "core_calendar_delete_calendar_events");
+//    print(json_encode($res));
+//}
+//
+//$date = new DateTime();
+//$date->add(new DateInterval("P2D"));
+//$d = date(DATE_RSS, $date->getTimestamp());
+//$aparams = [
+//    "events" =>[
+//        [
+//            "name" => "Esdeveniment de prova ($d)",
+//            "timestart" => $date->getTimestamp(),
+//            "courseid" => "2077",
+//            "eventtype" => "course",
+//            "description" => "Això és una prova per actualitzar el calendari des de la WIKI"
+//        ]
+//    ]
+//];
+//$res = $ws->sendRequest($aparams, "core_calendar_create_calendar_events");
+//print(json_encode($res));
+//
+//$ws = new WsMoodleCalendar();
+//
+//$ws->setToken($token);
+////$res = $ws->getEventsForCourseId("2077");
+////print(json_encode($res));
+//
+//$ws->deleteAllCourseEvents("2077");
+//$events = [];
+//$date = new DateTime();
+//$date->add(new DateInterval("P2D"));
+//$d = date(DATE_RSS, $date->getTimestamp());
+//$events[] = EventMoodle::newInstanceFromAssociative(array(
+//            "name" => "Esdeveniment de prova ($d)",
+//            "timestart" => $date->getTimestamp(),
+//            "eventtype" => "course",
+//            "description" => "Això és una prova per actualitzar el calendari des de la WIKI"
+//        ));
+//$date->add(new DateInterval("P1D"));
+//$d = date(DATE_RSS, $date->getTimestamp());
+//$events[] = EventMoodle::newInstanceFromAssociative(array(
+//            "name" => "Esdeveniment de prova ($d)",
+//            "timestart" => $date->getTimestamp(),
+//            "eventtype" => "course"
+//        ));
+//$ws->createEventsForCourseId("2077", $events);
 
 
 //MIX
 
 //courseid=10714
 
+$token2 = "f4b357ae6dabe3874681e07113a4d65d";
+$wsMix = new WsMixClient();
+
+$wsMix->setToken($token);
+
+$aparams = [
+    "courseid" =>10714
+];
+
+$res = $wsMix->sendRequest($aparams, "get_lessons_by_course");
+print("Course id: ".$aparams["courseid"]."<br>");
+print("Token: ".$token."<br>");
+print(json_encode($res));
+
+$wsMix->setToken($token2);
+try{
+    $res = $wsMix->getCourseLessons($aparams["courseid"]);
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print(json_encode($res));
+}catch(Exception $ex){
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print("Error: ".$ex->getCode()." - ".$ex->getMessage());
+}
+
+$wsMix->setToken($token);
+
+$aparams = [
+    "courseid" =>10724
+];
+
+$res = $wsMix->sendRequest($aparams, "get_lessons_by_course");
+print("<br>Course id: ".$aparams["courseid"]."<br>");
+print("Token: ".$token."<br>");
+print(json_encode($res));
+
+$wsMix->setToken($token2);
+try{
+    $res = $wsMix->getCourseLessons($aparams["courseid"]);
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print(json_encode($res));
+}catch(WsMixException $ex){
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print("Error: ".$ex->getCode()." - ".$ex->getMessage());
+}
+
+$wsMix->setToken($token);
+
+$aparams = [
+    "courseid" =>10946
+];
+
+$res = $wsMix->sendRequest($aparams, "get_lessons_by_course");
+print("<br>Course id: ".$aparams["courseid"]."<br>");
+print("Token: ".$token."<br>");
+print(json_encode($res));
+
+$wsMix->setToken($token2);
+try{
+    $res = $wsMix->getCourseLessons($aparams["courseid"]);
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print(json_encode($res));
+}catch(WsMixException $ex){
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print("Error: ".$ex->getCode()." - ".$ex->getMessage());
+}
+
+$wsMix->setToken($token);
+
+$aparams = [
+    "courseid" =>11433
+];
+
+$res = $wsMix->sendRequest($aparams, "get_lessons_by_course");
+print("<br>Course id: ".$aparams["courseid"]."<br>");
+print("Token: ".$token."<br>");
+print(json_encode($res));
+
+$wsMix->setToken($token2);
+try{
+    $res = $wsMix->getCourseLessons($aparams["courseid"]);
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print(json_encode($res));
+}catch(Exception $ex){
+    print("<br>Course id: ".$aparams["courseid"]."<br>");
+    print("Token: ".$token2."<br>");
+    print("Error: ".$ex->getCode()." - ".$ex->getMessage());
+}
--- wikiiocmodel/datamodel/MoodleProjectModel.php
+++ wikiiocmodel/datamodel/MoodleProjectModel.php
@@ -6,43 +6,72 @@
 if (!defined('DOKU_INC')) die();
 
 abstract class MoodleProjectModel extends AbstractProjectModel{
-
-    function sendCalendarDates($token){
-        $ws = new WsMoodleCalendar();
-        $ws->setToken($token);
-
-        $courseId = $this->getCourseId();
-        $oldEvents = EventMoodle::getListFromJson($ws->getEvents(array($courseId)));
-        //control d'errors
-
-        $events  = [];
-        foreach ($oldEvents->events as $item){
-            if($item->getEventType()=="course" && $item->getCourseId()==$courseId){
-                $events[] =$item;
-            }
+    private $moodleToken= FALSE;
+    
+    public function init($params, $projectType=NULL, $rev=NULL, $viewConfigKey=ProjectKeys::KEY_VIEW_DEFAULTVIEW, $metaDataSubSet=Projectkeys::VAL_DEFAULTSUBSET, $actionCommand=NULL, $isOnView=FALSE) {
+        parent::init($params, $projectType, $rev, $viewConfigKey, $metaDataSubSet, $actionCommand, $isOnView);
+        if(isset($params["moodleToken"]) && $params["moodleToken"]){
+            $this->moodleToken = $params["moodleToken"];
         }
-
-        if(count($events)>0){
-            $resp = $ws->deleteEventsFromEvents($events);
-            //control d'errors
-        }
-
-        $dates = $this->getCalendarDates();
-        $events = [];
-        foreach ($dates as $item) {
-            $events[] = [
-                "name" => $item["title"],
-                "timestart" => strptime($item["date"], "%Y-%m-%d"),
-                "courseid" => $courseId,
-                "eventtype" => "course",
-                "description" => $item['description']
-            ];
-        }
-
-        $resp = $ws->createEvents($events);
-        //control d'errors
-
     }
+    
+    public function getMoodleToken() {
+        return $this->moodleToken;
+    }
+    
+    protected function getMixDataLessons($courseId){
+        $res = FALSE;
+        if($courseId && $this->getMoodleToken()){
+            $wsMix = new WsMixClient();
+            $wsMix->setToken($this->getMoodleToken());
+            error_log("D0.1.- MoodleToquen:-".$this->getMoodleToken()."-");
+            error_log("D0.2.- CourseId:-".$courseId."-");
+            try{
+                $res = $wsMix->getCourseLessons($courseId);
+            }catch(WsMixException $ex){
+                error_log("E0.1.- Error: ".$ex->getMessage());
+                $res = FALSE;
+            }            
+        }        
+        return $res;
+    }
+
+//    function sendCalendarDates($token){
+//        $ws = new WsMoodleCalendar();
+//        $ws->setToken($token);
+//
+//        $courseId = $this->getCourseId();
+//        $oldEvents = EventMoodle::getListFromJson($ws->getEvents(array($courseId)));
+//        //control d'errors
+//
+//        $events  = [];
+//        foreach ($oldEvents->events as $item){
+//            if($item->getEventType()=="course" && $item->getCourseId()==$courseId){
+//                $events[] =$item;
+//            }
+//        }
+//
+//        if(count($events)>0){
+//            $resp = $ws->deleteEventsFromEvents($events);
+//            //control d'errors
+//        }
+//
+//        $dates = $this->getCalendarDates();
+//        $events = [];
+//        foreach ($dates as $item) {
+//            $events[] = [
+//                "name" => $item["title"],
+//                "timestart" => strptime($item["date"], "%Y-%m-%d"),
+//                "courseid" => $courseId,
+//                "eventtype" => "course",
+//                "description" => $item['description']
+//            ];
+//        }
+//
+//        $resp = $ws->createEvents($events);
+//        //control d'errors
+//
+//    }
 
     public abstract function getCourseId();
 
