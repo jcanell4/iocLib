0867095d2495c9ca7b1f7d28a274ac5264983d13
rclaver <rclaver@xtec.cat>
2023-05-08 09:08:34 +0200

    Minúscules pels noms de documents d'activitats

--- common/utility/CalculateLowerCaseInObjectArrayValues.php
+++ common/utility/CalculateLowerCaseInObjectArrayValues.php
@@ -4,16 +4,14 @@
  * @culpable rafa
  */
 class CalculateLowerCaseInObjectArrayValues extends CalculateFromValues {
-    const ARRAY_OBJECT_FIELD_PARAM = "arrayObjectfield";
-    const FIELD_TO_ADD_PARAM = "fieldToAdd";
 
     public function calculate($data) {
-        $values = $this->getVariable('values');
+        $values = $this->getValues();
         $table = $this->castToArray($values[$data['table']]);
         foreach ($table as $k => $row) {
             $table[$k][$data['field']] = strtolower($row[$data['field']]);
         }
-        return $table;
+        return json_encode($table);
     }
 
 }
