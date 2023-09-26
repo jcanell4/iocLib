b193c69323bbc7ca78e2ec4443a5be2a7dd6ea99
rclaver <rclaver@xtec.cat>
2023-09-26 14:53:34 +0200

    accesori per mostrar un array de noms d'autor a l'atribut 'nom_real'

--- common/utility/CalculateRealNameFromUsername.php
+++ common/utility/CalculateRealNameFromUsername.php
@@ -8,7 +8,11 @@ class CalculateRealNameFromUsername extends CalculateFromValues {
     public function calculate($data) {
         $name = $this->getParamValue($data);
         $user = $this->getValues()[$name];
-        $nom_real = PagePermissionManager::getUserList($user)['values'][0]['name'];
+        if (strpos($user, ",")) {
+            $nom_real = PagePermissionManager::getUsers($user);
+        }else {
+            $nom_real = PagePermissionManager::getUserList($user)['values'][0]['name'];
+        }
         return $nom_real;
     }
 }
